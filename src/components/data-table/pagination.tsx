0f79e94b3158ed940f29cb833e8d81cfd6d21017
Andrii Andreichenko <87246480+ShrewdLuni@users.noreply.github.com>
2025-11-03 11:43:24 +0100

    feat: add data-table pagination

---
+++ src/components/data-table/pagination.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button"
+import type { Table } from "@tanstack/react-table"
+
+interface TablePaginationProps<TData> {
+  table: Table<TData>
+}
+
+export function TablePagination<TData>({ table }: TablePaginationProps<TData>) {
+  return (
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="text-muted-foreground flex-1 text-sm">
+          {table.getFilteredSelectedRowModel().rows.length} of{" "}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
+        </div>
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+  )
+}
