49f50fcc461e7f5dbd39bb3267a7430b66e95161
Andrii Andreichenko <87246480+ShrewdLuni@users.noreply.github.com>
2025-11-25 01:02:26 +0100

    Feat: UseProducts hook

---
+++ src/hooks/useProducts.ts
@@ -0,0 +1,35 @@
+import type { Product } from "@/components/products/columns"
+import { API_URL } from "@/config"
+import { useEffect, useState } from "react"
+
+export const useProducts = () => {
+  const [products, setProducts] = useState<Product[]>([])
+  const [loading, setLoading] = useState<Boolean>(true)
+  const [error, setError] = useState<Error | null>(null)
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        setLoading(true)
+
+        const res = await fetch(`${API_URL}/products`, { credentials: "include" });
+
+        if(!res.ok) {
+          throw new Error("Failed to fetch products");
+        }
+        const data: Product[] = await res.json();
+        setProducts(data);
+
+      } catch (error: any) {
+        console.log(error);
+        setError(error);
+        setProducts([]);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getProducts();
+  }, [])
+
+  return { products, loading, error}
+}
