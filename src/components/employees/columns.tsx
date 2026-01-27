e53ec318ce09fae18b7752b458d1ddfdc947a84a
Andrii Andreichenko <87246480+ShrewdLuni@users.noreply.github.com>
2026-01-27 14:29:20 +0100

    feat: employees columns update

--- src/components/employees/columns.tsx
+++ src/components/employees/columns.tsx
@@ -4,101 +4,135 @@ import { IconCircleFilled } from "@tabler/icons-react"
 import { SortableHeader } from "../data-table/sortable-header";
 import { Checkbox } from "../ui/checkbox";
 import type { User } from "@/types/users";
+import { SelectCell } from "../data-table/select-cell";
 
-export const columns: ColumnDef<User>[] = [
-{
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
+type SelectCellOptions = {
+    label: string;
+    value: string;
+    icon?: any;
+}[]
+
+interface UserColumnsProps {
+  roleSelect: SelectCellOptions;
+  genderSelect: SelectCellOptions;
+  departmentsSelect: SelectCellOptions;
+  onCellUpdate: (field: string, value: string, row: any) => void;
+}
+
+export const getUserColumns = ({ roleSelect, genderSelect, departmentsSelect, onCellUpdate }: UserColumnsProps) => {
+
+  const columns: ColumnDef<User>[] = [
   {
-    accessorKey: "id",
-    header: ({ column }) => {
-      return (
-        <SortableHeader column={column} field={"ID"}/>
-      )
+      id: "select",
+      header: ({ table }) => (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select all"
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+        />
+      ),
+      enableSorting: false,
+      enableHiding: false,
     },
-  },
-  {
-    accessorKey: "role",
-    header: ({ column }) => {
-      return (
-        <SortableHeader column={column} field={"Role"}/>
-      )
+    {
+      accessorKey: "id",
+      header: ({ column }) => {
+        return (
+          <SortableHeader column={column} field={"ID"}/>
+        )
+      },
     },
-    cell: ({ row }) => {
-      return (
-        <p>{row.original.role == null ? "Not set" : row.original.role}</p>
-      )
+    {
+      accessorKey: "role",
+      header: ({ column }) => {
+        return (
+          <SortableHeader column={column} field={"Role"}/>
+        )
+      },
+      cell: ({ row }) => {
+        return (
+          <SelectCell row={row} defaultValue={row.original.role} data={roleSelect} placeholder="Assign role" onChange={(value) => onCellUpdate("role", value, row)}/>
+        )
+      },
     },
-  },
-  {
-    accessorKey: "fullName",
-    header: ({ column }) => {
-      return (
-        <SortableHeader column={column} field={"Name"}/>
-      )
+    {
+      accessorKey: "department",
+      header: ({ column }) => {
+        return (
+          <SortableHeader column={column} field={"Department"}/>
+        )
+      },
+      cell: ({ row }) => {
+        return (
+          <SelectCell row={row} defaultValue={row.original.department} data={departmentsSelect} placeholder="Assign department" onChange={(value) => onCellUpdate("department", value, row)}/>
+        )
+      },
     },
-  },
-  {
-    accessorKey: "email",
-    header: ({ column }) => {
-      return (
-        <SortableHeader column={column} field={"Email"}/>
-      )
+
+    {
+      accessorKey: "fullName",
+      header: ({ column }) => {
+        return (
+          <SortableHeader column={column} field={"Name"}/>
+        )
+      },
     },
-  },
-  {
-    accessorKey: "phone",
-    header: ({ column }) => {
-      return (
-        <SortableHeader column={column} field={"Phone"}/>
-      )
+    {
+      accessorKey: "email",
+      header: ({ column }) => {
+        return (
+          <SortableHeader column={column} field={"Email"}/>
+        )
+      },
     },
-  },
-  {
-    accessorKey: "gender",
-    header: ({ column }) => {
-      return (
-        <SortableHeader column={column} field={"Gender"}/>
-      )
+    {
+      accessorKey: "phone",
+      header: ({ column }) => {
+        return (
+          <SortableHeader column={column} field={"Phone"}/>
+        )
+      },
     },
-    cell: ({ row }) => (
-      <Badge variant="outline" className="text-muted-foreground px-1.5">
-        {row.original.gender === "Male" ? (
-          <IconCircleFilled className="fill-blue-700 dark:fill-blue-600" />
-        ) : (row.original.gender === "Female" ? (
-          <IconCircleFilled className="fill-pink-600 dark:fill-pink-500" />
-        ) : 
-         <IconCircleFilled className="fill-yellow-400 dark:fill-yellow-400" />
-          )}
-        {row.original.gender}
-      </Badge>
-    ),
-  },
-  {
-    accessorKey: "dateOfBirth",
-    header: ({ column }) => {
-      return (
-        <SortableHeader column={column} field={"Date Of Birth"}/>
-      )
+
+    {
+      accessorKey: "gender",
+      header: ({ column }) => {
+        return (
+          <SortableHeader column={column} field={"Gender"}/>
+        )
+      },
+      cell: ({ row }) => (
+          <SelectCell row={row} defaultValue={row.original.gender} data={genderSelect} placeholder="Assign gender" onChange={(value) => onCellUpdate("gender", value, row)}/>
+        // <Badge variant="outline" className="text-muted-foreground px-1.5">
+        //   {row.original.gender === "Male" ? (
+        //     <IconCircleFilled className="fill-blue-700 dark:fill-blue-600" />
+        //   ) : (row.original.gender === "Female" ? (
+        //     <IconCircleFilled className="fill-pink-600 dark:fill-pink-500" />
+        //   ) : 
+        //    <IconCircleFilled className="fill-yellow-400 dark:fill-yellow-400" />
+        //     )}
+        //   {row.original.gender}
+        // </Badge>
+      ),
     },
-  },
-]
+    {
+      accessorKey: "dateOfBirth",
+      header: ({ column }) => {
+        return (
+          <SortableHeader column={column} field={"Date Of Birth"}/>
+        )
+      },
+    },
+  ]
+  return columns;
+}
--- src/components/employees/index.tsx
+++ src/components/employees/index.tsx
@@ -1,12 +1,24 @@
-import { useUsers } from "@/hooks/useUsers";
 import { DataTable } from "../data-table";
 import { EmployeeForm } from "../forms/employee";
-import { columns } from "./columns"
+import { getUserColumns} from "./columns"
 import { Mars, Venus, CircleSmall, HardHat, UserStar, UserCog} from "lucide-react";
+import { useUsers } from "@/hooks/useUsers";
 
 export const EmployeesPage = () => {
 
-  const { users } = useUsers()
+
+  const { data: users, isLoading } = useUsers.getAll()
+  const { mutate: updateUser } = useUsers.update()
+
+  const handleCellUpdate = (field: string, value: string, row: any) => {
+    updateUser({
+      id: row.original.id,
+      data: {
+        ...row.original,
+        [field]: value,
+      }
+    })
+  }
 
   const gender = [
     {
@@ -29,27 +41,66 @@ export const EmployeesPage = () => {
   const roles = [
     {
       label: "Worker",
-      value: "worker",
+      value: "Worker",
       icon: HardHat,
     },
     {
       label: "Manager",
-      value: "manager",
+      value: "Manager",
       icon: UserStar,
     },
     {
       label: "Master",
-      value: "master",
+      value: "Master",
       icon: UserCog,
     },
+  ]
+
+  const departments = [
+    {
+      label: "Knitting",
+      value: "Knitting",
+    },
+    {
+      label: "Sewing",
+      value: "Sewing",
+    },
+    {
+      label: "Molding",
+      value: "Molding",
+    },
+    {
+      label: "Labeling",
+      value: "Labeling",
+    },
+    {
+      label: "Packaging",
+      value: "Packaging",
+    }
   ]
 
   const filters = [
     {column: "role", title: "Role", options: roles,},
     {column: "gender", title: "Gender", options: gender,}
   ]
+
+  if (isLoading) {
+    return <div>Is loading</div>
+  }
  
   return (
-    <DataTable columns={columns} data={users} contentForm={<EmployeeForm/>} filters={filters}/>
+    <DataTable 
+      columns={
+        getUserColumns({
+          roleSelect: roles, 
+          genderSelect: gender, 
+          departmentsSelect: departments,
+          onCellUpdate: handleCellUpdate,
+        })
+      } 
+      data={users ? users : []} 
+      contentForm={<EmployeeForm/>} 
+      filters={filters} 
+      searchValues={"fullName"}/>
   )
 }
