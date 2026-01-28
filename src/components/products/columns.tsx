3c44eb3a99cb955828a6b83fe94fdfbc903eb36c
Andrii Andreichenko <87246480+ShrewdLuni@users.noreply.github.com>
2026-01-28 12:47:43 +0100

    feat: products columns update

--- src/components/products/columns.tsx
+++ src/components/products/columns.tsx
@@ -4,78 +4,92 @@ import { type ColumnDef } from "@tanstack/react-table"
 import { SortableHeader } from "../data-table/sortable-header"
 import { Checkbox } from "@/components/ui/checkbox";
 import type { Product } from "@/types/products";
+import { CheckBoxCell } from "../data-table/checkbox-cell";
 
-export const columns: ColumnDef<Product>[] = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
-    accessorKey: "id",
-    header: ({ column }) => {
-      return (
-          <SortableHeader column={column} field={"ID"}/>
-      )
-    }
-  },
-  {
-    accessorKey: "code",
-    header: ({ column }) => {
-      return (
-        <SortableHeader column={column} field={"Code"}/>
-      )
-    }
-  },
-  {
-    accessorKey: "name",
-    header: ({ column }) => {
-      return (
-        <SortableHeader column={column} field={"Name"}/>
-      )
-    }
-  },
-  {
-    accessorKey: "isActive",
-    header: ({ column }) => {
-      return (
-        <SortableHeader column={column} field={"Is Active"}/>
-      )
-    }
-  },
+interface ProductColumnsProps {
+  onCellUpdate: (field: string, value: string | boolean, row: any) => void;
+}
 
-  {
-    accessorKey: "category",
-    header: ({ column }) => {
-      return (
-        <SortableHeader column={column} field={"Category"}/>
-      )
-    }
-  },
-  {
-    accessorKey: "measureUnit",
-    header: ({ column }) => {
-      return (
-        <SortableHeader column={column} field={"Unit"}/>
-      )
-    }
-  },
-]
+export const getProductColumns = ({ onCellUpdate }: ProductColumnsProps) => {
+  const columns: ColumnDef<Product>[] = [
+    {
+      id: "select",
+      header: ({ table }) => (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select all"
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+        />
+      ),
+      enableSorting: false,
+      enableHiding: false,
+    },
+    {
+      accessorKey: "id",
+      header: ({ column }) => {
+        return (
+            <SortableHeader column={column} field={"ID"}/>
+        )
+      }
+    },
+    {
+      accessorKey: "code",
+      header: ({ column }) => {
+        return (
+          <SortableHeader column={column} field={"Code"}/>
+        )
+      }
+    },
+    {
+      accessorKey: "name",
+      header: ({ column }) => {
+        return (
+          <SortableHeader column={column} field={"Name"}/>
+        )
+      }
+    },
+    {
+      accessorKey: "isActive",
+      header: ({ column }) => {
+        return (
+          <SortableHeader column={column} field={"Is Active"}/>
+        )
+      },
+      cell: ({ row }) => {
+        return <CheckBoxCell row={row} field="isActive" defaultValue={row.original.isActive} onChange={onCellUpdate}/>
+      },
+      filterFn: (row, id, value) => {
+        const cellValue = String(row.getValue(id));
+        return value.includes(cellValue);
+      },
+    },
+    {
+      accessorKey: "category",
+      header: ({ column }) => {
+        return (
+          <SortableHeader column={column} field={"Category"}/>
+        )
+      }
+    },
+    {
+      accessorKey: "measureUnit",
+      header: ({ column }) => {
+        return (
+          <SortableHeader column={column} field={"Unit"}/>
+        )
+      }
+    },
+  ]
 
+  return columns;
+}
--- src/components/products/index.tsx
+++ src/components/products/index.tsx
@@ -1,13 +1,61 @@
-import { useProducts } from "@/hooks/useProducts"
 import { DataTable } from "../data-table"
-import { columns } from "./columns"
+import { getProductColumns } from "./columns"
 import { BatchForm } from "../forms/batch"
+import { useProducts } from "@/hooks/useProduct"
+import { CircleCheck, CircleX } from "lucide-react"
 
 export const ProductsPage = () => {
 
-  const { products } = useProducts()
+  const { data: products, isLoading } = useProducts.getAll() 
+  const { mutate: updateProduct } = useProducts.update()
+
+  const handleCellUpdate = (field: string, value: string | boolean, row: any) => {
+    updateProduct({
+      id: row.original.id,
+      data: {
+        ...row.original,
+        [field]: value,
+      }
+    })
+  }
+
+  if (isLoading) {
+    <div>Loading...</div>
+  }
+
+  console.log(products)
+
+  const isActiveFilter = {
+    column: "isActive", 
+    title: "Is active", 
+    options: [
+      {
+        label: "Active",
+        value: "true",
+        icon: CircleCheck,
+      },
+      {
+        label: "Inactive",
+        value: "false",
+        icon: CircleX,
+      }
+    ],
+  }
+
+  const filters = [isActiveFilter]
 
   return (
-    <DataTable columns={columns} data={products} contentForm={<BatchForm onSuccess={() => console.log("true")}/>}/>
+    <DataTable 
+      columns={getProductColumns({onCellUpdate: handleCellUpdate})} 
+      data={products ? products : []} 
+      contentForm={<BatchForm onSuccess={() => console.log("true")}/>} 
+      searchValues="name" 
+      filters={filters}
+      initialState={{
+        columnFilters: [
+          { id: "isActive", value: ["true"] }
+        ]
+      }}
+    />
   )
 }
