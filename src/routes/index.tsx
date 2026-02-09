c481023b2e44dd3282806c93a0896f4f75ed8566
Andrii Andreichenko <87246480+ShrewdLuni@users.noreply.github.com>
2026-02-09 15:28:59 +0100

    fix: routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,31 @@
 import { AuthenticationPage } from "@/components/authentication";
 import { BatchPage } from "@/components/batch";
 import { EmployeesPage } from "@/components/employees";
-import { Preview } from "@/components/preview";
+import { PlanPage } from "@/components/plan";
+import { BatchPreviewPage } from "@/components/batch-preview";
 import { ProductsPage } from "@/components/products";
+import { QrCodeGenerationPage } from "@/components/qr-generation";
 import { WorkerPlanPage } from "@/components/worker-plan";
+import { WorkstationsPage } from "@/components/workstations";
+
 import {
+  Bolt,
   Boxes,
-  FileBox,
-  ListTodo,
+  ClipboardList,
+  LandPlot,
+  Package,
   PackageCheck,
   PackageOpen,
-  PackagePlus,
-  ScanBarcode,
+  QrCode,
+  // ScanEye,
   Users,
+  type LucideProps,
 } from "lucide-react";
+import { WorkstationPreviewPage } from "@/components/workstation-preview";
+import { QRCodePreviewPage } from "@/components/qrcode-preview";
+// import { Questioner } from "@/components/questioner";
+import { EmptyPage } from "@/components/empty";
+import type { ForwardRefExoticComponent, JSX, RefAttributes } from "react";
 
 export interface Route {
   name: string;
@@ -24,20 +36,21 @@ export interface Route {
   layout?: boolean;
 }
 
+// ('Superuser', 'Master', 'Manager', 'Worker', 'Observer'); 
+
 const loginRoute = {
   name: "Authentication",
   path: "/auth",
-  element: <AuthenticationPage/>,
+  element: <AuthenticationPage />,
   roles: ["*"],
   icon: Users,
-  layout: false,
-}
+};
 
 const employeesRoute = {
   name: "Employees",
   path: "/employees",
   element: <EmployeesPage />,
-  roles: ["manager", "observer", "superuser"],
+  roles: ["Master", "Manager", "Observer", "Superuser"],
   icon: Users,
   layout: true,
 };
@@ -46,7 +59,7 @@ const productsRoute = {
   name: "Products",
   path: "/products",
   element: <ProductsPage />,
-  roles: ["manager", "master", "observer", "superuser"],
+  roles: ["Master", "Manager", "Observer", "Superuser"],
   icon: Boxes,
   layout: true,
 };
@@ -55,74 +68,146 @@ const inProgressBatchesRoute = {
   name: "In Progress",
   path: "/batch/in-progress",
   element: <BatchPage />,
-  roles: ["manager", "master", "observer", "superuser"],
+  roles: ["Manager", "Master", "Observer", "Superuser"],
   icon: PackageOpen,
   layout: true,
 };
 
 const doneBatchesRoute = {
-  name: "Done",
-  path: "/batch/done",
+  name: "Batches",
+  path: "/batch",
   element: <BatchPage />,
-  roles: ["manager", "observer", "superuser"],
-  icon: PackageCheck,
+  roles: ["Manager", "Master", "Observer", "Superuser"],
+  icon: Package,
   layout: true,
 };
 
-const previewRoute = {
-  name: "Preview",
-  path: "/batch/preview",
-  element: <Preview/>,
+const singularBatchRoute = {
+  name: "Scan",
+  path: "/batch/:id/",
+  element: <BatchPreviewPage/>,
   roles: ["*"],
-  icon: PackageOpen,
-  layout: false,
+  icon: PackageCheck,
+};
+
+const qrcodeRoute = {
+  name: "Generate QR",
+  path: "/qrcodes",
+  element: <QrCodeGenerationPage />,
+  roles: ["Manager", "Master", "Observer", "Superuser"],
+  icon: QrCode,
+  layout: true,
+};
+
+const singularQRCodeRoute = {
+  name: "Scan",
+  path: "/qrcodes/:id/",
+  element: <QRCodePreviewPage/>,
+  roles: ["*"],
+  icon: PackageCheck,
+};
+
+const workerPlanRoute = {
+  name: "Worker Plan",
+  path: "/woker/plan",
+  element: <WorkerPlanPage/>,
+  roles: ["Worker"],
+  icon: ClipboardList, 
+  layout: true,
 }
 
-const createBatchRotue = {
-  name: "Initialize",
-  path: "/batch/create",
-  element: <BatchPage/>,
-  roles: ["manager", "observer", "superuser"],
-  icon: PackagePlus 
+const workstationsRoute = {
+  name: "Workstations",
+  path: "/workstations",
+  element: <WorkstationsPage/>,
+  roles: ["Manager", "Master", "Observer", "Superuser"],
+  icon: Bolt, 
+  layout: true,
 }
 
-// const planRoute = {
-//   name: "Plan",
-//   path: "/plan",
-//   element: <WorkerPlanPage />,
-//   roles: ["worker", "manager", "master", "observer", "superuser"],
-//   icon: ListTodo,
-// };
-//
-// const scanRoute = {
-//   name: "Scan",
-//   path: "/scan",
-//   element: <WorkerPlanPage />,
-//   roles: ["worker", "manager", "master", "observer", "superuser"],
-//   icon: ScanBarcode,
-// };
-//
+const emptyRoute = {
+  name: "empty",
+  path: "/",
+  element: <EmptyPage/>,
+  roles: ["*"],
+  icon: Bolt, 
+  layout: true,
+}
+
+const singularWorkstationRoute = {
+  name: "Scan",
+  path: "/workstations/:id/",
+  element: <WorkstationPreviewPage/>,
+  roles: ["*"],
+  icon: PackageCheck,
+};
+
+const planRoute = {
+  name: "Plan",
+  path: "/plan",
+  element: <PlanPage/>,
+  roles: ["Manager", "Master", "Observer", "Superuser"],
+  icon: LandPlot, 
+  layout: true,
+}
+
+// const actionsRoute = {
+//   name: "Actions",
+//   path: "/actions",
+//   element: <Questioner/>,
+//   roles: ["Manager", "Master", "Observer", "Superuser"],
+//   icon: ScanEye, 
+//   layout: true,
+// }
 
 export const routes: Route[] = [
   employeesRoute,
   productsRoute,
   inProgressBatchesRoute,
   doneBatchesRoute,
-  // createBatchRotue,
-  // planRoute,
-  // scanRoute,
   loginRoute,
-  previewRoute
+  qrcodeRoute,
+  singularQRCodeRoute,
+  singularBatchRoute,
+  workerPlanRoute,
+  workstationsRoute,
+  singularWorkstationRoute,
+  planRoute,
+  // actionsRoute,
+  emptyRoute,
 ];
 
-export const sidebarElements = [
-  employeesRoute,
-  productsRoute,
-  {
-    group: "Batches",
-    icon: FileBox,
-    items: [inProgressBatchesRoute, doneBatchesRoute],
-  },
-  // planRoute,
-  // scanRoute,
-];
+export const sidebarElements: ({
+  name: string;
+  path: string;
+  element: JSX.Element;
+  roles: string[];
+  icon: ForwardRefExoticComponent<Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>>;
+  layout: boolean;
+} | {
+    group: string;
+    icon: any;
+    items: {
+      name: string;
+      path: string;
+      element: JSX.Element;
+      roles: string[];
+      icon: ForwardRefExoticComponent<Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>>;
+      layout: boolean;
+    }[];
+  })[] = [
+    employeesRoute,
+    productsRoute,
+    workstationsRoute,
+    // {
+    //   group: "Batches",
+    //   icon: FileBox,
+    //   items: [inProgressBatchesRoute, doneBatchesRoute],
+    // },
+    doneBatchesRoute,
+    planRoute,
+    qrcodeRoute,
+    // actionsRoute,
+    emptyRoute,
+    workerPlanRoute,
+  ];
