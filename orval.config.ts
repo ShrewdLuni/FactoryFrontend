80c33442f6361ee18ced1591f7a33b8132e2ab22
Andrii Andreichenko <87246480+shrewddd@users.noreply.github.com>
2026-05-20 12:36:52 +0200

    feat: add orval config & custom fetch

---
+++ orval.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "orval";
+
+export default defineConfig({
+  api: {
+    input: "http://192.168.0.101:3000/openapi.json",
+    output: {
+      mode: "tags-split",
+      target: "src/api/generated",
+      schemas: "src/api/generated/models",
+      client: "react-query",
+      httpClient: "fetch",
+      override: {
+        mutator: { path: "src/api/fetcher.ts", name: "customFetch" },
+        fetch: {
+          includeHttpResponseReturnType: false, // 👈 add this
+        },
+      },
+    },
+  },
+});
---
+++ src/api/fetcher.ts
@@ -0,0 +1,10 @@
+import { API_URL } from "@/config";
+
+export const customFetch = async <T>(url: string, options?: RequestInit): Promise<T> => {
+  const res = await fetch(`${API_URL}/${url}`, {
+    ...options,
+    headers: { "Content-Type": "application/json", ...options?.headers },
+  });
+  if (!res.ok) throw new Error(await res.text());
+  return res.json();
+};
